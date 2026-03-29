67f8d254eb7b14df3cccdeba7d9d7882286a4e83
Trang-nny <trang.nny03322@sinhvien.hoasen.edu.vn>
2026-03-29 20:44:43 +0700

    refactor: cập nhật giao diện Home nhập PIN chuẩn Figma và thêm font Irish Grover

--- Quiz-frontend/src/app/pages/home/home.ts
+++ Quiz-frontend/src/app/pages/home/home.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
-export class HomeComponent {
-  quizzes = [
-    { title: 'General Knowledge', questions: 10 },
-    { title: 'Science Quiz', questions: 8 },
-    { title: 'Technology Basics', questions: 12 },
-    { title: 'Music Trivia', questions: 6 },
-    { title: 'Geography Test', questions: 9 }
-  ];
-}
+export class HomeComponent {}
