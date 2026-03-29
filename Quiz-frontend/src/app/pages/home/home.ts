9764968318a3593866d95f235d1990103920ef5f
Trang-nny <trang.nny03322@sinhvien.hoasen.edu.vn>
2026-03-29 20:12:16 +0700

    feat: hoàn thiện giao diện trang home

--- Quiz-frontend/src/app/pages/home/home.ts
+++ Quiz-frontend/src/app/pages/home/home.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
-  styleUrls: ['./home.css'],
+  styleUrls: ['./home.css']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  quizzes = [
+    { title: 'General Knowledge', questions: 10 },
+    { title: 'Science Quiz', questions: 8 },
+    { title: 'Technology Basics', questions: 12 },
+    { title: 'Music Trivia', questions: 6 },
+    { title: 'Geography Test', questions: 9 }
+  ];
+}
