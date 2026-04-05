2621408bfeecb5d71fe78efb22d2f154ccd4edce
Trang-nny <trang.nny03322@sinhvien.hoasen.edu.vn>
2026-04-05 19:57:14 +0700

    feat: implement create quiz UI with responsive layout and styling

--- Quiz-frontend/src/app/features/quiz/create-quiz/create-quiz.ts
+++ Quiz-frontend/src/app/features/quiz/create-quiz/create-quiz.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create-quiz',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './create-quiz.html',
-  styleUrl: './create-quiz.css',
+  styleUrls: ['./create-quiz.css']
 })
-export class CreateQuiz {}
+export class CreateQuiz {
+  // Logic xử lý thêm câu hỏi, lưu quiz sẽ được Backend xử lý sau
+}
