4a12d23f416d587e05c73a3b1d503f78f7728ca0
Trang-nny <trang.nny03322@sinhvien.hoasen.edu.vn>
2026-04-06 17:31:01 +0700

    feat: Complete the Mode Selection interface with a 2-column layout and hover effects.

--- Quiz-frontend/src/app/features/game/mode-selection/mode-selection.ts
+++ Quiz-frontend/src/app/features/game/mode-selection/mode-selection.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-mode-selection',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './mode-selection.html',
-  styleUrl: './mode-selection.css',
+  styleUrls: ['./mode-selection.css']
 })
-export class ModeSelection {}
+export class ModeSelection {
+  constructor() {}
+}
--- Quiz-frontend/src/app/layouts/game-layout/game-layout.ts
+++ Quiz-frontend/src/app/layouts/game-layout/game-layout.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router'; 
 
 @Component({
   selector: 'app-game-layout',
-  imports: [],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './game-layout.html',
-  styleUrl: './game-layout.css',
+  styleUrls: ['./game-layout.css'],
 })
 export class GameLayout {}
