9b0fe94b6c7586527b77676988e45074449420f1
Trang-nny <trang.nny03322@sinhvien.hoasen.edu.vn>
2026-04-11 13:36:47 +0700

    feat: hoàn thành giao diện màn hình edit profile

--- Quiz-frontend/src/app/features/profile-edit/profile-edit.ts
+++ Quiz-frontend/src/app/features/profile-edit/profile-edit.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-edit',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './profile-edit.html',
   styleUrl: './profile-edit.css',
 })
-export class ProfileEdit {}
+export class ProfileEdit {
+  user = {
+    username: 'Alex Rivera',
+    accountId: 'ID_882941',
+    email: 'alex.rivera@example.com',
+    bio: 'Passionate quizzer and knowledge seeker. Exploring the Neon Atoll one trivia at a time! \uD83D\uDE80',
+    avatar: 'User.png'
+  };
+
+  saveChanges() {
+    console.log('Saving changes:', this.user);
+  }
+
+  cancel() {
+    console.log('Cancel editing');
+  }
+}
